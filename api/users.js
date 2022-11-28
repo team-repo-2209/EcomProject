89f444ddca020c5d3d430495a386e4b54d82a21f
David Masoud <davidmasoud24@gmail.com>
2022-11-28 14:28:03 -0800

    Backend almost done

--- api/users.js
+++ api/users.js
@@ -5,7 +5,74 @@ const prisma = require("../prisma/prisma");
 router.get(
   "/",
   asyncErrorHandler(async (req, res, next) => {
-    const orders = await prisma.orders.findMany();
-    res.send(orders);
+    const users = await prisma.users.findMany();
+    res.send(users);
   })
 );
+
+router.post(
+  "/",
+  asyncErrorHandler(async (req, res, next) => {
+    const createUser = await prisma.users.create({
+      data: req.body,
+    });
+    res.send(newUser);
+  })
+);
+
+router.patch(
+  "/:userId",
+  asyncErrorHandler(async (req, res, next) => {
+    const updateUser = await prisma.users.update({
+      where: {
+        id: +req.params.userId,
+      },
+      data: req.body,
+    });
+    res.send(updateUser);
+  })
+);
+
+router.fetch(
+  "/:userId",
+  asyncErrorHandler(async (req, res, next) => {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: +req.params.orderId,
+      },
+    });
+    res.send(user);
+  })
+);
+
+router.delete(
+  "/:userId",
+  asyncErrorHandler(async (req, res, next) => {
+    const deleteUser = await prisma.users.delete({
+      where: {
+        id: +req.params.userId,
+      },
+    });
+    res.send(deleteUser);
+  })
+);
+
+router.post("/login", asyncErrorHandler, async (req, res, next) => {
+  const { username, password } = data;
+  const user = await prisma.users.findUnique({
+    where: {
+      username,
+    },
+  });
+  if (!user) {
+    throw createError.NotFound("User is not registered");
+  }
+  const checkPassword = bcrypt.compareSync(password, user.password);
+  if (!checkPassword)
+    throw createError.Unauthorized("Username or password is not valid");
+  delete user.password;
+  const accessToken = await jwt.signAccessToken(user);
+  return { ...user, accessToken };
+});
+
+module.exports = router;
