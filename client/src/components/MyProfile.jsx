aed36305a6830756b3bd111e308a8abff58087ba
Cynthia <cluk@dynamicsignal.com>
2022-11-30 21:36:17 -0800

    my profile component

--- client/src/components/MyProfile.jsx
+++ client/src/components/MyProfile.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import useUsers from "../hooks/UseUsers";
-import { fetchMe } from "../api/users";
+import { fetchMe, updateUser } from "../api/users";
 
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
@@ -27,68 +27,86 @@ export default function MyProfile() {
     }
     getMyProfile();
   }, [user]);
+
   return (
-    <From>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Username</Form.Label>
-        <Form.Control
-          value={username}
-          type="text"
-          placeholder={user.username}
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-        <Form.Label>Email Address</Form.Label>
-        <Form.Control
-          value={email}
-          type="email"
-          placeholder={user.email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          value={password}
-          type="text"
-          placeholder={user.password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <Form.Label>Firstname</Form.Label>
-        <Form.Control
-          value={firstname}
-          type="text"
-          placeholder={user.firstname}
-          onChange={(e) => {
-            setFirstname(e.target.value);
-          }}
-        />
-        <Form.Label>Lastname</Form.Label>
-        <Form.Control
-          value={lastname}
-          type="text"
-          placeholder={user.lastname}
-          onChange={(e) => {
-            setLastname(e.target.value);
-          }}
-        />
-        <Form.Label>Phone Number</Form.Label>
-        <Form.Control
-          value={phoneNumber}
-          type="text"
-          placeholder={user.phoneNumber}
-          onChange={(e) => {
-            setPhoneNumber(e.target.value);
-          }}
-        />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-      <Button variant="secondary">Change</Button>
-    </From>
+    <div>
+      <Form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          window.location.reload();
+          const result = await updateUser(
+            user.id,
+            email,
+            password,
+            firstname,
+            lastname,
+            phoneNumber
+          );
+          console.log("Updated Profile: ", result);
+        }}
+      >
+        <h3>My Profile</h3>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            value={username}
+            type="text"
+            placeholder={user.username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            value={email}
+            type="email"
+            placeholder={user.email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            value={password}
+            type="text"
+            placeholder={user.password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <Form.Label>Firstname</Form.Label>
+          <Form.Control
+            value={firstname}
+            type="text"
+            placeholder={user.firstname}
+            onChange={(e) => {
+              setFirstname(e.target.value);
+            }}
+          />
+          <Form.Label>Lastname</Form.Label>
+          <Form.Control
+            value={lastname}
+            type="text"
+            placeholder={user.lastname}
+            onChange={(e) => {
+              setLastname(e.target.value);
+            }}
+          />
+          <Form.Label>Phone Number</Form.Label>
+          <Form.Control
+            value={phoneNumber}
+            type="text"
+            placeholder={user.phoneNumber}
+            onChange={(e) => {
+              setPhoneNumber(e.target.value);
+            }}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Button variant="secondary">Change</Button>
+      </Form>
+    </div>
   );
 }
