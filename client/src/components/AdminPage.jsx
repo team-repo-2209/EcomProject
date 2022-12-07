0ce1d23a9d4557c0ece2bd7c69cc9e4563ff864a
Cynthia <cluk@dynamicsignal.com>
2022-12-07 10:06:37 -0800

    added create product to admin

--- client/src/components/AdminPage.jsx
+++ client/src/components/AdminPage.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import useUsers from "../hooks/useUsers";
 import { fetchAllUsers } from "../api/users";
+import CreateProduct from "./CreateProduct";
 
 import styles from "../styles/Register.module.css";
 import Card from "react-bootstrap/Card";
@@ -11,6 +12,7 @@ export default function AdminPage() {
   const { user } = useUsers();
   const [profiles, setProfiles] = useState([]);
   const navigate = useNavigate();
+  const [showCreate, setShowCreate] = useState(false);
 
   useEffect(() => {
     async function getAllUsers() {
@@ -22,24 +24,36 @@ export default function AdminPage() {
     console.log("Profiles: ", profiles);
   }, []);
 
+  function displayCreate() {
+    setShowCreate(true);
+  }
+
   return (
-    <div className={styles.container}>
-      {profiles?.map((profile) => {
-        return (
-          <Card
-            key={profile.id}
-            style={{ width: "30rem" }}
-            className={styles.card}
-          >
-            <h6>
-              Name: {profile.firstname} {profile.lastname}
-            </h6>
-            <h6>Username: {profile.username}</h6>
-            <h6>Email Address: {profile.email}</h6>
-            <h6>Phone Number: {profile.phoneNumber}</h6>
-          </Card>
-        );
-      })}
+    <div>
+      <div>
+        <button onClick={displayCreate}>Create A new Product</button>
+        {showCreate === true ? <CreateProduct /> : null}
+      </div>
+
+      <div className={styles.container}>
+        <h3>User Information</h3>
+        {profiles?.map((profile) => {
+          return (
+            <Card
+              key={profile.id}
+              style={{ width: "30rem" }}
+              className={styles.card}
+            >
+              <h6>
+                Name: {profile.firstname} {profile.lastname}
+              </h6>
+              <h6>Username: {profile.username}</h6>
+              <h6>Email Address: {profile.email}</h6>
+              <h6>Phone Number: {profile.phoneNumber}</h6>
+            </Card>
+          );
+        })}
+      </div>
     </div>
   );
 }
